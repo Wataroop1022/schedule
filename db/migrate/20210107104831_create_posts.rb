c0e3830d84c162a86989d926db7c31cb34237f25
USER <user@USERnoMacBook-Air.local>
2021-01-07 22:09:59 +0900

    complete

---
+++ db/migrate/20210107104831_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.date :day_start
+      t.date :day_end
+      t.boolean :all_day
+      t.text :memo
+
+      t.timestamps
+    end
+  end
+end
